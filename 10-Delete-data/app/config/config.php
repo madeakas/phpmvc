9fcf8cc43803f262a0fe1582700aba3493c00084
Made Akas <66621940+madeakas@users.noreply.github.com>
2022-04-16 01:59:12 +0700

    Delete_Data

---
+++ 10-Delete-data/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// BASEURL
+define('BASEURL', 'http://localhost/phpmvc/10-Delete-data/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ 10-Delete-data/app/views/templates/footer.php
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="<?php echo BASEURL; ?>/js/bootstrap.js"></script>
+</body>
+
+</html>
