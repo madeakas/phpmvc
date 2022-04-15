9e96ec44240c9c7581b885c0878ca95dff4c1b70
Made Akas <66621940+madeakas@users.noreply.github.com>
2022-04-16 01:56:20 +0700

    Flash_Message

---
+++ 09-Flash-message/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// BASEURL
+define('BASEURL', 'http://localhost/phpmvc/09-Flash-message/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
