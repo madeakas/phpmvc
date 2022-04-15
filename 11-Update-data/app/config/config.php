c7845413f78a05834faa9d65dd006d422eeed39b
Made Akas <66621940+madeakas@users.noreply.github.com>
2022-04-16 02:01:04 +0700

    Update_Data

---
+++ 11-Update-data/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// BASEURL
+define('BASEURL', 'http://localhost/phpmvc/11-Update-data/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
---
+++ 11-Update-data/app/views/templates/footer.php
@@ -0,0 +1,8 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.5/umd/popper.min.js"></script>
+<!-- <script src="<?php echo BASEURL; ?>/js/popper.min.js"></script> -->
+<script src="<?php echo BASEURL; ?>/js/jquery-3.6.0.js"></script>
+<script src="<?php echo BASEURL; ?>/js/bootstrap.js"></script>
+<script src="<?php echo BASEURL; ?>/js/script.js"></script>
+</body>
+
+</html>
