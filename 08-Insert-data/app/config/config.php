36ea664baeca107b7c92872c442af568ad2a919c
Made Akas <66621940+madeakas@users.noreply.github.com>
2022-04-16 01:54:07 +0700

    Insert

---
+++ 08-Insert-data/app/config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+// BASEURL
+define('BASEURL', 'http://localhost/phpmvc/08-Insert-data/public');
+
+// DB
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'phpmvc');
